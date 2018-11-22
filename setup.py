420cafc2da5e330ca0e767d6ff7f0026f3cd8851
frkhit <frkhit@gmail.com>
2018-11-22 23:36:28 +0800

    init project

---
+++ setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+ver_dic = {}
+version_file = open("version.py")
+try:
+    version_file_contents = version_file.read()
+finally:
+    version_file.close()
+
+exec(compile(version_file_contents, "version.py", 'exec'), ver_dic)
+
+setup(name="pytools",
+      version=ver_dic["VERSION_TEXT"],
+      description="simple tool for Python3.6",
+      long_description=open("README.rst", "r").read(),
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Other Audience',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Utilities',
+      ],
+
+      install_requires=[
+          "numpy>=1.6.0",
+      ],
+
+      author="frkhit",
+      url="https://github.com/frkhit/pytools",
+      author_email="frkhit@gmail.com",
+      license="MIT",
+      packages=["pytools"])
