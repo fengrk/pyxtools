e82ddf30fa14150f31e1905b9275bcac332dc761
frkhit <frkhit@gmail.com>
2018-11-25 20:30:27 +0800

    add git tools

--- pytools/dev_tools/crlf_tools.py
+++ pytools/dev_tools/crlf_tools.py
@@ -5,13 +5,15 @@ import logging
 import os
 import shutil
 
+CRLF_VALUE = {"lf": "\n", "crlf": "\r\n", "cr": "\r"}
+
 
 def change_crlf_mode_of_file(file_name, dst_lf):
     tmp_file = file_name + ".bak"
     shutil.copy(file_name, tmp_file, follow_symlinks=False)
 
-    with open(tmp_file, 'r', newline=None) as infile, \
-            open(file_name, 'w', newline=dst_lf) as outfile:
+    with open(tmp_file, 'r', newline=None, encoding="utf-8") as infile, \
+            open(file_name, 'w', newline=dst_lf, encoding="utf-8") as outfile:
         outfile.writelines(infile.readlines())
 
     os.remove(tmp_file)
@@ -33,4 +35,4 @@ def change_to_crlf(file_list: list):
             logging.error(e, exc_info=True)
 
 
-__all__ = ("change_to_crlf", "change_to_lf", "change_crlf_mode_of_file")
+__all__ = ("change_to_crlf", "change_to_lf", "change_crlf_mode_of_file", "CRLF_VALUE")
--- pytools/dev_tools/git_tools.py
+++ pytools/dev_tools/git_tools.py
@@ -1,22 +1,74 @@
 # -*- coding:utf-8 -*-
 from __future__ import absolute_import
 
+import fnmatch
+import json
+import logging
 import os
+import subprocess
+import sys
+from optparse import OptionParser
 from subprocess import check_output
 
+import requests
 
-def git_list_status_file() -> list:
-    """ list all file showed in `git status -s` """
-    raw_lines = check_output(["git", "status", "-s"]).decode("utf-8").split("\n")
+from .crlf_tools import change_crlf_mode_of_file, CRLF_VALUE
+from ..basic_tools import global_init_logger
 
-    def _get_file_name(_line: str) -> str:
-        blank_index = _line.find(" ")
-        if blank_index > -1:
-            return _line[blank_index:]
-        return ""
+logger = logging.getLogger(__name__)
 
-    lines = [_get_file_name(clear_line) for clear_line in [line.strip("\r").strip() for line in raw_lines]]
-    return [file_name for file_name in [line.strip() for line in lines] if len(file_name) > 0]
+
+def git_list_status_file(path) -> list:
+    """ list all file showed in `cd path && git status -s` """
+    raw_path = os.getcwd()
+    try:
+        os.chdir(path)
+        raw_lines = check_output(["git", "status", "-s"]).decode("utf-8").split("\n")
+        logger.info("result of `git status -s` is {}".format("\n".join(raw_lines)))
+
+        def _get_file_name(_line: str) -> str:
+            blank_index = _line.find(" ")
+            if blank_index > -1:
+                return _line[blank_index:]
+            return ""
+
+        lines = [_get_file_name(clear_line) for clear_line in [line.strip("\r").strip() for line in raw_lines]]
+        file_list = [os.path.join(path, file_name) for
+                     file_name in [line.strip() for line in lines] if len(file_name) > 0]
+        logger.info("result of `git_list_status_file` is ['{}']".format("'\n'".join(file_list)))
+        return file_list
+    finally:
+        os.chdir(raw_path)
+
+
+def git_crlf_fatal_file(path) -> list:
+    """ list all file showed in `cd path && git status -s` """
+    raw_path = os.getcwd()
+    try:
+        os.chdir(path)
+        try:
+            output = check_output(["git", "add", "."], shell=True, stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as e:
+            output = e.output
+
+        raw_lines = output.decode("utf-8").split("\n")
+        logger.info("result of `git add .` is {}".format("\n".join(raw_lines)))
+        fatal_line = raw_lines[0].strip()
+
+        file_list = []
+        if fatal_line.find("fatal") > -1:
+            file_name = os.path.join(path, fatal_line.split(" ")[-1].rstrip("."))
+            if os.path.exists(file_name) and os.path.isfile(file_name):
+                file_list = [file_name]
+            else:
+                logger.warning("{} not exists!".format(file_name))
+        if file_list:
+            logger.info("result of `git_crlf_fatal_file` is ['{}']".format("'\n'".join(file_list)))
+        else:
+            logger.info("result of `git_crlf_fatal_file` is []")
+        return file_list
+    finally:
+        os.chdir(raw_path)
 
 
 def is_git_repo_log_dir(log_dir: str) -> bool:
@@ -44,3 +96,77 @@ def find_git_log_dir(path: str) -> (str, str):
                             return path, real_git_log
 
         path = os.path.join(path, "..")
+
+
+def git_crlf_helper():
+    global_init_logger()
+    hstr = '%prog custom help string'
+    parser = OptionParser(hstr, description='custom description', version='%prog 1.0')
+    parser.add_option('-d', '--dir', action='store', dest='dir', default=".", help='working dir')
+    parser.add_option('-i', '--include', action='store', dest='include', default="",
+                      help='include pattern, join by `,`')
+    parser.add_option('-e', '--exclude', action='store', dest='exclude', default="",
+                      help='exclude pattern, join by `,`')
+    parser.add_option('-t', '--target', action='store', dest='target', default="lf",
+                      help='target: crlf, lf, cr')
+    if sys.argv[0] == "-c":
+        argv = list(sys.argv)
+        argv[0] = __file__
+        options, args = parser.parse_args(argv)
+    else:
+        options, args = parser.parse_args(sys.argv)
+
+    if options.target in CRLF_VALUE:
+        options.target = CRLF_VALUE[options.target]
+
+    include = None
+    if len(options.include) > 0:
+        include = [pattern for pattern in options.include.split(",") if len(pattern) > 0]
+
+    file_list = git_list_status_file(options.dir)
+    all_file_list = []
+    for file_name in file_list:
+        if not os.path.exists(file_name):
+            logger.warning("{} not exists, ignore it".format(file_name))
+        else:
+            all_file_list.append(file_name)
+    file_list = list(all_file_list)
+
+    if include is not None and len(include) > 0:
+        all_file_list = []
+        for pattern in include:
+            all_file_list.extend(fnmatch.filter(file_list, pattern))
+        file_list = list(set(all_file_list))
+
+    exclude = None
+    if len(options.exclude) > 0:
+        exclude = [pattern for pattern in options.exclude.split(",") if len(pattern) > 0]
+    if exclude is not None and len(exclude) > 0:
+        exclude_file_list = []
+        for pattern in exclude:
+            exclude_file_list.extend(fnmatch.filter(file_list, pattern))
+        file_list = list(set(file_list) - set(exclude_file_list))
+
+    if len(file_list) == 0:
+        logger.info("not file in unstage status!")
+        return
+
+    [change_crlf_mode_of_file(file_name, options.target) for file_name in file_list]
+
+    count = 100
+    file_list = git_crlf_fatal_file(options.dir)
+    while count > 0 and len(file_list) > 0:
+        change_crlf_mode_of_file(file_list[0], options.target)
+        file_list = git_crlf_fatal_file(options.dir)
+        count -= 1
+
+
+def md2html_by_github(content: str) -> str:
+    """ use github api """
+    logger.info("requesting github...")
+    url = "https://api.github.com/markdown"
+    return requests.post(url, data=json.dumps({"text": content, "mode": "markdown"})).text
+
+
+__all__ = ("git_list_status_file", "is_git_repo_log_dir", "git_crlf_fatal_file",
+           "find_git_log_dir", "md2html_by_github", "git_crlf_helper")
