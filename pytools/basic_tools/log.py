074b6b0e7656caeb3ea9fa55f215aad7d3bdb094
frkhit <frkhit@gmail.com>
2018-12-08 00:30:27 +0800

    [basic_tools] log: add filehandler support

--- pytools/basic_tools/log.py
+++ pytools/basic_tools/log.py
@@ -3,61 +3,9 @@ from __future__ import absolute_import
 
 import logging
 import sys
-import time
+from logging.handlers import TimedRotatingFileHandler, RotatingFileHandler
 
 _inited_logger_args = None
-
-
-class DailyRotatingHandler(logging.FileHandler):
-    def __init__(self, filename, max_bytes, mode='a', encoding='utf-8'):
-        super(DailyRotatingHandler, self).__init__(filename, mode, encoding, delay=1)
-        self.fn_prefix = self.baseFilename
-        self.st = None
-        self.max_bytes = max_bytes
-
-    def _open_file(self):
-        self.st = time.localtime()
-        suffix = time.strftime('%Y-%m-%d', self.st)
-        self.baseFilename = '%s.%s' % (self.fn_prefix, suffix)
-        return self._open()
-
-    def should_skip(self, record):
-        if self.stream is None:
-            self.stream = self._open_file()
-
-        if self.max_bytes > 0:
-            msg = "%s\n" % self.format(record)
-            self.stream.seek(0, 2)
-            if self.stream.tell() + len(msg) >= self.max_bytes:
-                return 1
-        return 0
-
-    def should_rollover(self, record):
-        if self.stream is None:
-            self.stream = self._open_file()
-
-        st = time.localtime()
-        if st.tm_mday != self.st.tm_mday or st.tm_mon != self.st.tm_mon or st.tm_year != self.st.tm_year:
-            return 1
-        return 0
-
-    def do_rollover(self):
-        if self.stream:
-            self.stream.close()
-            self.stream = None
-        self.stream = self._open_file()
-
-    def emit(self, record):
-        try:
-            if self.should_rollover(record):
-                self.do_rollover()
-            if self.should_skip(record):
-                return
-            self.emit(record)
-        except (KeyboardInterrupt, SystemExit):
-            raise
-        except:
-            self.handleError(record)
 
 
 def _clear_logger_handlers(_logger):
@@ -67,7 +15,7 @@ def _clear_logger_handlers(_logger):
 
 
 def _logger_config_is_same(used_args, to_use_args) -> bool:
-    if used_args[0] != to_use_args[0] or used_args[2] != to_use_args[2]:
+    if used_args[0] != to_use_args[0] or used_args[2] != to_use_args[2] or used_args[3] != to_use_args[3]:
         return False
 
     if isinstance(used_args[1], list) and isinstance(to_use_args[1], list):
@@ -80,11 +28,22 @@ def _logger_config_is_same(used_args, to_use_args) -> bool:
     return used_args[1] == to_use_args[1]
 
 
-def global_init_logger(logger_level=logging.INFO, log_file: str = None, reset_logger_name_list: list = None):
+def get_time_rotating_handler(log_file: str) -> logging.FileHandler:
+    fh = TimedRotatingFileHandler(log_file, when='midnight', interval=1, backupCount=30)
+    return fh
+
+
+def get_rotating_file_handler(log_file: str) -> logging.FileHandler:
+    fh = RotatingFileHandler(log_file, maxBytes=2 * 1024 * 1024 * 1024, backupCount=30)
+    return fh
+
+
+def global_init_logger(logger_level=logging.INFO, log_file: str = None, reset_logger_name_list: list = None,
+                       log_file_handler: logging.FileHandler = None):
     global _inited_logger_args
 
-    if _inited_logger_args is not None and _logger_config_is_same(_inited_logger_args,
-                                                                  (log_file, reset_logger_name_list, logger_level)):
+    if _inited_logger_args is not None and _logger_config_is_same(_inited_logger_args, (
+            log_file, reset_logger_name_list, logger_level, log_file_handler)):
         logging.warning("init logger run before!")
         return
 
@@ -106,10 +65,12 @@ def global_init_logger(logger_level=logging.INFO, log_file: str = None, reset_lo
     root_logger.addHandler(_console_handler)
 
     # file handler
-    if log_file is not None:
-        _file_handler = logging.FileHandler(log_file)
-        _file_handler.setFormatter(_default_formatter)
-        root_logger.addHandler(_file_handler)
+    if log_file_handler is None and log_file is not None:
+        log_file_handler = logging.FileHandler(log_file)
+
+    if log_file_handler is not None:
+        log_file_handler.setFormatter(_default_formatter)
+        root_logger.addHandler(log_file_handler)
 
     root_logger.setLevel(logger_level)
 
@@ -119,4 +80,4 @@ def global_init_logger(logger_level=logging.INFO, log_file: str = None, reset_lo
         _inited_logger = (log_file, list(reset_logger_name_list), logger_level)
 
 
-__all__ = ("global_init_logger", "DailyRotatingHandler")
+__all__ = ("global_init_logger", "get_time_rotating_handler", "get_rotating_file_handler")
